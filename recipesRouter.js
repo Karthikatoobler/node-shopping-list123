3b8c936f2738c16d9a55e9c558e7e36aa908b402
Karthika P J <karthikapj.tblr@gmail.com>
2018-03-09 16:28:44 +0530

    build error12

--- recipesRouter.js
+++ recipesRouter.js
@@ -33,7 +33,7 @@ router.get('/', (req, res) => {
 router.post('/', jsonParser, (req, res) => {
   // ensure `name` and `budget` are in request body
   // const requiredFields = ['name', 'ingredients'];
-  const requiredFields = '';
+  const requiredFields = ''
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
